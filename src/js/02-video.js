62ac957c8e0d8d3283d0f69a2e88462c77b53ff6
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-05-29 18:40:52 +0300

    Done 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const keyStorage = "videoplayer-current-time"
+    
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(function(data) {
+        localStorage.setItem(keyStorage, JSON.stringify(data.seconds))
+    }), 1000);
+    seconds = JSON.parse(localStorage.getItem(keyStorage))
+
+   player.setCurrentTime(seconds).then(function(seconds) {
+       
+     
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+           
+            break;
+
+        default:
+          
+            break;
+    }
+});
+   
