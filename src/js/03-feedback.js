ed434227ee6fc3f48d46f9eca309c61ed6a9b989
Drobot777 <121822078+Drobot777@users.noreply.github.com>
2023-05-30 13:30:36 +0300

    Done 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,26 @@
-
+import throttle from 'lodash.throttle';
+const formEl = document.querySelector('.feedback-form')
+const keyStorage = "feedback-form-state"
+const formData = JSON.parse(localStorage.getItem(keyStorage)) || {};
+  formEl.addEventListener('input', throttle(onFormData, 500))
+    formEl.addEventListener('submit', onSubmitForm);
+function onFormData(e) {
+     formData[e.target.name] = e.target.value;
+   localStorage.setItem(keyStorage, JSON.stringify(formData));
+}
+function onSubmitForm(e) {
+       console.log(JSON.parse(localStorage.getItem(keyStorage)));
+   e.preventDefault();
+   e.currentTarget.reset();
+   localStorage.removeItem(keyStorage);
+}
+(function dataFromLocalStorage() {
+  
+  const data = JSON.parse(localStorage.getItem(keyStorage));
+  const { email, message } = formEl.elements;
+  if (data) {
+    
+    email.value = data.email || '';
+    message.value = data.message || '';
+  }
+})();
